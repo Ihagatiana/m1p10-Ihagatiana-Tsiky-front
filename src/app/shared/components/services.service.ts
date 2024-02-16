4def9b1365951e07adfe3711899796bb3e9b6b73
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-16 14:39:24 +0300

    [ADD] Api request on get services list

--- src/app/shared/components/services.service.ts
+++ src/app/shared/components/services.service.ts
@@ -10,15 +10,15 @@ export class ServicesService {
   private base_url = environment.baseUrl;
   constructor(private readonly httpService: HttpClient) {}
   getAll() {
-    return this.httpService.get<Service[]>(`${this.base_url}/service/all`);
+    return this.httpService.get<Service[]>(`${this.base_url}/services`);
   }
 
   findOne(id: Service['id']) {
-    return this.httpService.get<Service>(`${this.base_url}/service/${id}`);
+    return this.httpService.get<Service>(`${this.base_url}/services/${id}`);
   }
 
   create(service: Service) {
-    return this.httpService.post<Service>(`${this.base_url}/service`, service);
+    return this.httpService.post<Service>(`${this.base_url}/services`, service);
   }
 
   update(id: Service['id'], service: UpdateServiceDto) {
@@ -28,7 +28,7 @@ export class ServicesService {
     );
   }
   delete(id: Service['id']) {
-    return this.httpService.delete<Service>(`${this.base_url}/${id}`);
+    return this.httpService.delete<Service>(`${this.base_url}/services/${id}`);
   }
 }
 
--- src/app/shared/components/services/services-list/services-list.component.ts
+++ src/app/shared/components/services/services-list/services-list.component.ts
@@ -9,7 +9,7 @@ import { Service, ServicesService } from '../../services.service';
 export class ServicesListComponent {
   loading = false;
   @Input() canSwitchView = false;
-  displayMode: 'list' | 'grid' = 'list';
+  displayMode: 'list' | 'grid' = 'grid';
   services: Service[] = [];
   constructor(private readonly service: ServicesService) {}
   ngOnInit(): void {
@@ -21,51 +21,9 @@ export class ServicesListComponent {
 
   fetchAll() {
     this.loading = true;
-    this.service
-      .getAll()
-      .subscribe((data) => {
-        this.services = data;
-        this.loading = false;
-      })
-      .unsubscribe();
-
-    // this.loading = true;
-    // setTimeout(() => {
-    //   this.services = [
-    //     {
-    //       id: 1,
-    //       name: 'Services 1',
-    //       duration: { hours: 0, minutes: 30 },
-    //       price: 500,
-    //       description:
-    //         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'Services 2',
-    //       duration: { hours: 1, minutes: 30 },
-    //       price: 450,
-    //       description:
-    //         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'Services 3',
-    //       duration: { hours: 2, minutes: 0 },
-    //       price: 600,
-    //       description:
-    //         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    //     },
-    //     {
-    //       id: 1,
-    //       name: 'Services 4',
-    //       duration: { hours: 0, minutes: 45 },
-    //       price: 1500,
-    //       description:
-    //         "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum",
-    //     },
-    //   ];
-    //   this.loading = false;
-    // }, 500);
+    this.service.getAll().subscribe((data) => {
+      this.services = data;
+      this.loading = false;
+    });
   }
 }
