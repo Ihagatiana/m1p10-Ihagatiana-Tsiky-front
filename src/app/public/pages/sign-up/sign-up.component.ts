459949068b5ebd6bcacb50edcc7b8eb68f2742d2
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-21 22:56:16 +0300

    [IMPL] Control on signup form

--- src/app/public/pages/sign-up/sign-up.component.ts
+++ src/app/public/pages/sign-up/sign-up.component.ts
@@ -1,10 +1,57 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   templateUrl: './sign-up.component.html',
-  styleUrls: ['./sign-up.component.scss']
+  styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent {
+  form: FormGroup;
+  loading = false;
 
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/),
+        ],
+      ],
+      password: ['', Validators.required],
+      confirmationPassword: ['', Validators.required],
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+    });
+  }
+
+  onSignUp() {
+    this.loading = true;
+    // console.log(this.form.value);
+
+    setTimeout(() => {
+      console.log(this.form.value);
+      this.loading = false;
+    }, 15000);
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  get confirmationPassword() {
+    return this.form.get('confirmationPassword');
+  }
+
+  get firstname() {
+    return this.form.get('firstname');
+  }
+  get lastname() {
+    return this.form.get('lastname');
+  }
 }
