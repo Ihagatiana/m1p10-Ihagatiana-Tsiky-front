6609a638188aa274a8aa53e60d6337ac55c114ba
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-22 16:59:57 +0300

    [IMPL] Side bar employee [IMPL] Layout for employee [IMPL] base page for employee

--- src/app/employee/employee-routing.module.ts
+++ src/app/employee/employee-routing.module.ts
@@ -1,7 +1,19 @@
+import { EmployeeAppointmentComponent } from './pages/employee-appointment/employee-appointment.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { EmployeeLayoutComponent } from './employee-layout/employee-layout.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: EmployeeLayoutComponent,
+    children: [
+      { path: '', component: ProfileComponent },
+      { path: 'appointments', component: EmployeeAppointmentComponent },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
---
+++ src/app/employee/pages/employee-appointment/employee-appointment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-appointment',
+  templateUrl: './employee-appointment.component.html',
+  styleUrls: ['./employee-appointment.component.scss']
+})
+export class EmployeeAppointmentComponent {
+
+}
