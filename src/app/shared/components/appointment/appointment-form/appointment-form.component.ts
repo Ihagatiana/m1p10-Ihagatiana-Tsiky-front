a63c57477ed860a01c36e6d02cc24e0a76c50171
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-25 22:43:35 +0300

    [IMPL] Creation form for appointement

--- src/app/shared/components/appointment/appointment-form/appointment-form.component.ts
+++ src/app/shared/components/appointment/appointment-form/appointment-form.component.ts
@@ -1,10 +1,49 @@
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-form',
   templateUrl: './appointment-form.component.html',
-  styleUrls: ['./appointment-form.component.scss']
+  styleUrls: ['./appointment-form.component.scss'],
 })
 export class AppointmentFormComponent {
+  form: FormGroup;
 
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      id: [null],
+      clients: [null, Validators.required],
+      date: [null, Validators.required],
+      state: [1],
+      starttime: [null],
+      appservices: this.formBuilder.array([]),
+    });
+  }
+
+  get clients() {
+    return this.form.get('clients');
+  }
+
+  get starttime() {
+    return this.form.get('starttime');
+  }
+
+  get appservices() {
+    return this.form.get('appservices') as FormArray;
+  }
+
+  addService() {
+    const service = this.appservices;
+    service.push(
+      this.formBuilder.group({
+        services: [null, Validators.required],
+        employes: [null, Validators.required],
+        order: [1, Validators.required],
+      })
+    );
+  }
+
+  submitForm() {
+    console.log(this.form.value);
+  }
 }
