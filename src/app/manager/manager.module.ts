2430367c594557f19ed15022bec254e264097f68
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-26 21:26:20 +0300

    [IMPL] Employee list

--- src/app/manager/manager.module.ts
+++ src/app/manager/manager.module.ts
@@ -1,14 +1,13 @@
+import { PaperComponent } from './../components/paper/paper.component';
+import { SharedModule } from './../shared/shared.module';
 import { ManagerRoutingModule } from './manager-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { ManagerLayoutComponent } from './manager-layout/manager-layout.component';
+import { EmployesComponent } from './pages/employes/employes.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ManagerRoutingModule
-  ]
+  declarations: [ManagerLayoutComponent, EmployesComponent],
+  imports: [CommonModule, ManagerRoutingModule, SharedModule, PaperComponent],
 })
-export class ManagerModule { }
+export class ManagerModule {}
---
+++ src/app/shared/components/employe/employe-form/employe-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employe-form',
+  templateUrl: './employe-form.component.html',
+  styleUrls: ['./employe-form.component.scss']
+})
+export class EmployeFormComponent {
+
+}
