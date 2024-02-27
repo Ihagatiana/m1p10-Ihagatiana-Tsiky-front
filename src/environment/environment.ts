0c5e72e3a10d8bc7a359a7d2b18a2c634af7dc3a
Tsiky <tsikyanjaranomena@gmail.com>
2024-02-27 23:26:57 +0300

    [FIX] Base_Url

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -5,5 +5,5 @@
 export const environment = {
   production: false,
   // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://m1p11-ihagatiana-tsiky-front.onrender.com',
+  baseUrl: 'https://beautysalon-belh.onrender.com',
 };
