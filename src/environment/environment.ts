670d21dbcd3ff2b8c3984e11442bcbd3f94da117
Ihagatiana <iramilijaona@ingenosya.mg>
2024-02-28 17:03:21 +0300

    [FIX] View based on role

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -4,6 +4,6 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://localhost:3000',
-  baseUrl: 'https://beautysalon-belh.onrender.com',
+  baseUrl: 'http://localhost:3000',
+  // baseUrl: 'https://beautysalon-belh.onrender.com',
 };
